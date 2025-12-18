e3cd8df0cf124f011a4cacdcfcb6e3533e878bcd
Shiroyasha <shiroyasha@3fee295fc007>
2025-12-18 19:49:15 +0700

    fix: update api/index.php for writable storage

--- api/index.php
+++ api/index.php
@@ -1,2 +1,19 @@
 <?php
+
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/bootstrap/cache',
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0755, true);
+    }
+}
+
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('CACHE_DIRECTORY=/tmp/storage/framework/cache');
+
 require __DIR__ . '/../public/index.php';
