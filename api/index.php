ed73352ea072cf7adefd0c12fe873312a23071a5
Shiroyasha <shiroyasha@3fee295fc007>
2025-12-18 20:20:39 +0700

    fix: clear cache and final config update

--- api/index.php
+++ api/index.php
@@ -1,18 +1,20 @@
 <?php
 
-$storageFolders = [
+// Paksa folder storage ke /tmp agar Laravel tidak crash saat menulis log/cache
+$basePath = __DIR__ . '/..';
+putenv("APP_STORAGE=/tmp/storage");
+
+// Buat folder yang dibutuhkan di /tmp
+$folders = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/bootstrap/cache',
 ];
-
-foreach ($storageFolders as $folder) {
+foreach ($folders as $folder) {
     if (!is_dir($folder)) {
         mkdir($folder, 0755, true);
     }
 }
-
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 
 require __DIR__ . '/../public/index.php';
